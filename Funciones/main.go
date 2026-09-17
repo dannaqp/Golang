3b89c1e457c232ea4addd985434d2df0a120f538
DANNA ALEJANDRA SIMALUISA QUIZHPI <dasimaluisaqu@uide.edu.ec>
2026-09-17 11:25:30 -0500

    ejercicios en clase

--- Funciones/main.go
+++ Funciones/main.go
@@ -21,6 +21,10 @@ func sumaresta(c, d int) (int, int) {
 	return c + d, 0
 }
 
+func mostrarNum(num ...int) {
+	fmt.Println("Los números ingresados son: ", num)
+}
+
 func main() {
 
 	var usr string
@@ -40,4 +44,16 @@ func main() {
 
 	w, z := sumaresta(c, d)
 	fmt.Println("El resultado de la suma es: ", w, "y de la resta es: ", z)
+
+	mostrarNum(5, 10, 15, 20, 25)
+
+	fmt.Println("La sumatoria es: ", sumatoria(1, 2, 3, 4, 5, 6, 7, 8, 9))
+}
+
+func sumatoria(nums ...int) int {
+	total := 0
+	for _, nro := range nums {
+		total += nro
+	}
+	return total
 }
