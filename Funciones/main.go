acf8cf08c863ef6fbe9601a1a215cae71537134c
DANNA ALEJANDRA SIMALUISA QUIZHPI <dasimaluisaqu@uide.edu.ec>
2026-09-17 10:39:50 -0500

    ejercicios

--- Funciones/main.go
+++ Funciones/main.go
@@ -1,1 +1,29 @@
 package main
+
+import "fmt"
+
+func saludar() {
+	fmt.Println("Hola esta es mi primera función")
+}
+
+func bienvenida(nombre string) {
+	fmt.Println("Bienvenid@", nombre)
+}
+
+func suma(a, b int) int {
+	return a + b
+}
+
+func main() {
+
+	var usr string
+	fmt.Println("Ingresa tu nombre: ")
+	fmt.Scan(&usr)
+	saludar()
+	bienvenida(usr)
+
+	var a, b int
+	fmt.Println("Ingresa dos valores: ")
+	fmt.Scan(&a, &b)
+	fmt.Println("El resultado es: ", suma(a, b))
+}
